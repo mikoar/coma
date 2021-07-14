fd3745c245555a86a32b8a287853b2e09d96f244
mikoar <myyq@interia.pl>
2021-07-14 22:33:33 +0200

    multiprocessing -> p_tqdm

--- src/script_stats.py
+++ src/script_stats.py
@@ -12,7 +12,7 @@ from sequence_generator import SequenceGenerator
 from worker import workerFunction
 from tqdm import tqdm
 from random import sample
-import multiprocessing
+from p_tqdm import p_map
 rcParams["lines.linewidth"] = 1
 rcParams['axes.prop_cycle'] = cycler(color=["#e74c3c"])
 
@@ -31,13 +31,12 @@ def getWorkerInputs(alignments: List[Alignment], reference: np.ndarray, queries:
 
 
 if __name__ == '__main__':
-    multiprocessing.freeze_support()
 
     alignmentsFile = "../data/EXP_REFINEFINAL1.xmap"
     referenceFile = "../data/hg19_NT.BSPQI_0kb_0labels.cmap"
     queryFile = "../data/EXP_REFINEFINAL1.cmap"
 
-    alignmentsCount = 10
+    alignmentsCount = 1000
     resolutions = [32, 48, 64, 256, 1024, 1536]
     blurs = [0, 2, 4, 8, 12]
 
@@ -46,32 +45,30 @@ if __name__ == '__main__':
     # %%
 
     isoResolutionResults = []
-    with multiprocessing.Pool(processes=6, maxtasksperchild=1) as pool:
+    with tqdm(total=alignmentsCount * len(blurs) * len(resolutions)) as progressBar:
+        for resolution in resolutions:
+            isoBlurResults = []
+            for blur in blurs:
+                sequenceGenerator = SequenceGenerator(resolution, blur)
+                reader = CmapReader(sequenceGenerator)
 
-        with tqdm(total=alignmentsCount * len(blurs) * len(resolutions)) as progressBar:
-            for resolution in resolutions:
-                isoBlurResults = []
-                for blur in blurs:
-                    sequenceGenerator = SequenceGenerator(resolution, blur)
-                    reader = CmapReader(sequenceGenerator)
+                validCount = 0
+                sampledAlignments = sample(alignments, alignmentsCount)
+                referenceIds = set(map(lambda a: a.referenceId, sampledAlignments))
+                alignmentsGroupedByReference = [[a for a in sampledAlignments if a.referenceId == r] for r in referenceIds]
+                for alignmentsForReference, referenceId in zip(alignmentsGroupedByReference, referenceIds):
+                    reference = reader.readReference(referenceFile, referenceId)
+                    queries = reader.readQueries(queryFile, list(map(lambda a: a.queryId, alignmentsForReference)))
+                    progressBar.set_description(
+                        f"Resolution: {resolution}, blur: {blur}, {len(queries)} queries for reference {referenceId}")
 
-                    validCount = 0
-                    sampledAlignments = sample(alignments, alignmentsCount)
-                    referenceIds = set(map(lambda a: a.referenceId, sampledAlignments))
-                    alignmentsGroupedByReference = [[a for a in sampledAlignments if a.referenceId == r] for r in referenceIds]
-                    for alignmentsForReference, referenceId in zip(alignmentsGroupedByReference, referenceIds):
-                        reference = reader.readReference(referenceFile, referenceId)
-                        queries = reader.readQueries(queryFile, list(map(lambda a: a.queryId, alignmentsForReference)))
-                        progressBar.set_description(
-                            f"Resolution: {resolution}, blur: {blur}, {len(queries)} queries for reference {referenceId}")
+                    poolResults = p_map(workerFunction, getWorkerInputs(alignmentsForReference, reference.sequence, queries, resolution), num_cpus=8)
+                    validCount += sum(poolResults)
+                    progressBar.update(len(alignmentsForReference))
 
-                        poolResults = pool.map(workerFunction, getWorkerInputs(alignmentsForReference, reference.sequence, queries, resolution))
-                        validCount += sum(poolResults)
-                        progressBar.update(len(alignmentsForReference))
+                isoBlurResults.append(validCount / len(sampledAlignments) * 100)
 
-                    isoBlurResults.append(validCount / len(sampledAlignments))
-
-                isoResolutionResults.append(isoBlurResults)
+            isoResolutionResults.append(isoBlurResults)
 
     plotHeatMap(isoResolutionResults, alignmentsCount, blurs, resolutions)
 
