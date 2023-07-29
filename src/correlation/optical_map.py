c6dcea07c9cd0b98d41ea98a71a05b9e16f2396d
mikoar <myyq@interia.pl>
2023-07-29 17:26:17 +0200

    CorrelationResult refactoring

--- src/correlation/optical_map.py
+++ src/correlation/optical_map.py
@@ -7,10 +7,8 @@ from typing import List
 import numpy as np
 from scipy.signal import find_peaks, correlate
 
-from src.correlation.bionano_alignment import BionanoAlignment
 from src.correlation.peak import Peak
 from src.correlation.sequence_generator import SequenceGenerator
-from src.diagnostic.validator import Validator
 
 
 @dataclass(frozen=True)
@@ -22,9 +20,9 @@ class PositionWithSiteId:
         return self.position < other.position
 
 
-def adjustPeakPositions(peakPositions: np.ndarray, resolution: int, start: int = 0) -> np.ndarray:
+def toRelativeGenomicPositions(correlationCoordinates: np.ndarray, resolution: int, start: int = 0) -> np.ndarray:
     resolutionAdjustment = ceil(resolution / 2) - 1
-    return peakPositions * resolution + (resolutionAdjustment + start)
+    return correlationCoordinates * resolution + (resolutionAdjustment + start)
 
 
 @dataclass(frozen=True)
@@ -72,9 +70,9 @@ class OpticalMap:
             rel_height=0.5,
             distance=((5 * 10 ** 6) / sequenceGenerator.resolution))
 
-        return InitialAlignment(correlation, self, reference, peakPositions, peakProperties, reverseStrand,
-                                sequenceGenerator.resolution, sequenceGenerator.blurRadius, 0,
-                                len(correlation) * sequenceGenerator.resolution)
+        return InitialAlignment.create(correlation, self, reference, peakPositions, peakProperties, reverseStrand,
+                                       sequenceGenerator.resolution, sequenceGenerator.blurRadius, 0,
+                                       len(correlation) * sequenceGenerator.resolution)
 
     def getSequence(self, sequenceGenerator: SequenceGenerator, reverseStrand=False, start: int = 0, end: int = None):
         sequence = sequenceGenerator.positionsToSequence(self.positions, start, end)
@@ -85,102 +83,97 @@ class OpticalMap:
         return correlate(reference, query, mode='valid', method='fft')
 
 
-@dataclass()
 class CorrelationResult:
-    correlation: np.ndarray
-    query: OpticalMap
-    reference: OpticalMap
-    peakPositions: np.ndarray
-    peakProperties: dict
-    reverseStrand: bool
-    resolution: int = 1
-    blur: int = 0
-    correlationStart: int = 0
-    correlationEnd: int = None
+    @staticmethod
+    def create(correlation: np.ndarray,
+               query: OpticalMap,
+               reference: OpticalMap,
+               peakPositions: np.ndarray,
+               peakProperties: dict,
+               reverseStrand: bool,
+               resolution: int = 1,
+               blur: int = 0,
+               correlationStart: int = 0,
+               correlationEnd: int = None):
+        return CorrelationResult(
+            correlation,
+            query,
+            reference,
+            CorrelationResult.createPeaks(peakPositions, peakProperties, resolution, correlationStart),
+            reverseStrand,
+            CorrelationResult.rootMeanSquare(correlation),
+            resolution,
+            blur,
+            correlationStart,
+            correlationEnd or len(correlation) - 1)
 
-    def __post_init__(self):
-        self.peakPositions = adjustPeakPositions(self.peakPositions, self.resolution, self.correlationStart)
-        self.peakProperties["left_ips"] = adjustPeakPositions(self.peakProperties["left_ips"], self.resolution,
-                                                              self.correlationStart)
-        self.peakProperties["right_ips"] = adjustPeakPositions(self.peakProperties["right_ips"], self.resolution,
-                                                               self.correlationStart)
-        self.noiseLevel = self.__rootMeanSquare(self.correlation)
-        if self.correlationEnd is None:
-            self.correlationEnd = len(self.correlation) - 1
+    def __init__(self,
+                 correlation: np.ndarray,
+                 query: OpticalMap,
+                 reference: OpticalMap,
+                 peaks: List[Peak],
+                 reverseStrand: bool,
+                 noiseLevel: float,
+                 resolution: int = 1,
+                 blur: int = 0,
+                 correlationStart: int = 0,
+                 correlationEnd: int = None):
+        self.correlation = correlation
+        self.query = query
+        self.reference = reference
+        self.peaks = peaks
+        self.reverseStrand = reverseStrand
+        self.noiseLevel = noiseLevel
+        self.resolution = resolution
+        self.blur = blur
+        self.correlationStart = correlationStart
+        self.correlationEnd = correlationEnd
 
     @staticmethod
-    def __rootMeanSquare(array: np.ndarray):
-        return np.sqrt(np.mean(array[array != 0] ** 2))
+    def createPeaks(peakPositions: np.ndarray, peakProperties: dict, resolution: int, correlationStart: int):
+        return [Peak(position, height, leftBase, rightBase)
+                for position, height, leftBase, rightBase
+                in zip(toRelativeGenomicPositions(peakPositions, resolution, correlationStart),
+                       toRelativeGenomicPositions(peakProperties["peak_heights"], resolution, correlationStart),
+                       toRelativeGenomicPositions(peakProperties["left_ips"], resolution, correlationStart),
+                       toRelativeGenomicPositions(peakProperties["right_ips"], resolution, correlationStart))]
 
-    def getRelativeScore(self, reference: BionanoAlignment, validator: Validator):
-        maxPeak = self.maxPeak
-        isMaxValid = validator.validate(maxPeak, reference)
-        if maxPeak and isMaxValid:
-            peakHeight = maxPeak.height
-        else:
-            peakHeight = self.__getMaxValidPeakHeight(reference,
-                                                      validator) or self.__getMaxCorrelationValueInAlignmentRange(
-                reference)
-        return self.__getScoreRelativeToNthPeak(peakHeight)
+    @staticmethod
+    def rootMeanSquare(array: np.ndarray):
+        return np.sqrt(np.mean(array[array != 0] ** 2))
 
     def getScore(self):
         return self.maxPeak.height - self.correlation.mean() if self.maxPeak else 0
 
     @property
     def maxPeak(self):
-        if not self.peakPositions.any():
-            return
-
-        maxIndex = np.argmax(self.__peakHeights)
-        return Peak(self.peakPositions[maxIndex], self.__peakHeights[maxIndex], self.__leftBases[maxIndex],
-                    self.__rightBases[maxIndex])
-
-    @property
-    def peaks(self):
-        for position, height, leftBase, rightBase in zip(self.peakPositions, self.__peakHeights, self.__leftBases,
-                                                         self.__rightBases):
-            yield Peak(position, height, leftBase, rightBase)
-
-    @property
-    def __peakHeights(self) -> np.ndarray:
-        return self.peakProperties["peak_heights"]
-
-    @property
-    def __leftBases(self) -> np.ndarray:
-        return self.peakProperties["left_ips"]
-
-    @property
-    def __rightBases(self) -> np.ndarray:
-        return self.peakProperties["right_ips"]
-
-    def __getMaxValidPeakHeight(self, reference: BionanoAlignment, validator: Validator):
-        validPeaks = [peak for peak in self.peaks if validator.validate(peak, reference)]
-        maxValidPeakHeight = max(validPeaks, key=lambda peak: peak.height).height if validPeaks else None
-        return maxValidPeakHeight
-
-    def __getMaxCorrelationValueInAlignmentRange(self, reference: BionanoAlignment) -> float:
-        expectedQueryStartPosition = int(reference.expectedQueryMoleculeStart / self.resolution)
-        expectedQueryEndPosition = int(reference.expectedQueryMoleculeStart / self.resolution)
-        expectedQueryRange: np.ndarray = self.correlation[
-                                         expectedQueryStartPosition: expectedQueryEndPosition]
-        return np.max(expectedQueryRange) if expectedQueryRange.any() else 0.
-
-    def __getScoreRelativeToNthPeak(self, peakHeight: float = None):
-        heights = self.__peakHeights
-        if len(heights) == 0:
-            return 0.
-
-        if len(heights) == 1:
-            return 1.
-
-        order = min(5, len(heights))
-
-        firstPeakHeight, nthPeakHeight = sorted(heights, reverse=True)[:order:order - 1]
-        peakHeightToScore = peakHeight if peakHeight else firstPeakHeight
-        return peakHeightToScore - nthPeakHeight
+        return max(self.peaks, key=lambda p: p.height, default=None)
 
 
 class InitialAlignment(CorrelationResult):
+    @staticmethod
+    def create(correlation: np.ndarray,
+               query: OpticalMap,
+               reference: OpticalMap,
+               peakPositions: np.ndarray,
+               peakProperties: dict,
+               reverseStrand: bool,
+               resolution: int = 1,
+               blur: int = 0,
+               correlationStart: int = 0,
+               correlationEnd: int = None):
+        return InitialAlignment(
+            correlation,
+            query,
+            reference,
+            InitialAlignment.createPeaks(peakPositions, peakProperties, resolution, correlationStart),
+            reverseStrand,
+            InitialAlignment.rootMeanSquare(correlation),
+            resolution,
+            blur,
+            correlationStart,
+            correlationEnd or len(correlation) - 1)
+
     def refine(self, sequenceGenerator: SequenceGenerator, secondaryMargin: int = 8000,
                peakHeightThreshold: float = 15.):
         querySequence = self.query.getSequence(sequenceGenerator, self.reverseStrand)
@@ -195,10 +188,10 @@ class InitialAlignment(CorrelationResult):
 
         correlationLength = len(correlation) * resolution
 
-        return CorrelationResult(correlation, self.query, self.reference, peakPositions, peakProperties,
-                                 self.reverseStrand, resolution, sequenceGenerator.blurRadius,
-                                 referenceStart,
-                                 referenceStart + correlationLength)
+        return CorrelationResult.create(correlation, self.query, self.reference, peakPositions, peakProperties,
+                                        self.reverseStrand, resolution, sequenceGenerator.blurRadius,
+                                        referenceStart,
+                                        referenceStart + correlationLength)
 
     @staticmethod
     def __getCorrelation(reference: np.ndarray, query: np.ndarray) -> np.ndarray:
@@ -211,6 +204,12 @@ class EmptyInitialAlignment(InitialAlignment):
                  reference: OpticalMap,
                  resolution: int,
                  blur: int):
+        super().__init__(np.array([]),
+                         query,
+                         reference,
+                         [],
+                         False,
+                         0.)
         self.query = query
         self.reference = reference
         self.resolution = resolution
@@ -218,7 +217,3 @@ class EmptyInitialAlignment(InitialAlignment):
 
     def getScore(self):
         return 0.
-
-    @property
-    def peaks(self):
-        return []
