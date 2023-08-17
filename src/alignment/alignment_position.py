2aeade1f249f56427dcf8ab070e5501e152c5ce6
zofiakk <zosia.kochanska@gmail.com>
2023-08-17 21:50:17 +0200

    Review changes

--- src/alignment/alignment_position.py
+++ src/alignment/alignment_position.py
@@ -160,24 +160,23 @@ class ScoredNotAlignedPosition(NotAlignedPosition, ScoredAlignmentPosition):
         return self.__position.absolutePosition
 
     def __init__(self, position: NotAlignedPosition, score: float):
-        self.__position = position
         self.score = score
         self.position = position
 
     def __repr__(self) -> str:
-        return f"{self.__position}, score:{self.score:.2f}"
+        return f"{self.position}, score:{self.score:.2f}"
 
     def __eq__(self, other: NotAlignedPosition):
-        return self.__position == other
+        return self.position == other
     
     def lessOnBothSequences(self, other: AlignedPair) -> bool:
-        if isinstance(self.__position, NotAlignedReferencePosition):
-            return self.__position.reference.position  < other.reference.position
+        if isinstance(self.position, NotAlignedReferencePosition):
+            return self.position.reference.position  < other.reference.position
         else:
-            return self.__position.query.position < other.query.position
+            return self.position.query.position < other.query.position
 
     def lessOrEqualOnAnySequence(self, other: AlignedPair) -> bool:
-        if isinstance(self.__position, NotAlignedReferencePosition):
-            return self.__position.reference.position  <= other.reference.position
+        if isinstance(self.position, NotAlignedReferencePosition):
+            return self.position.reference.position  <= other.reference.position
         else:
-            return self.__position.query.position <= other.query.position
+            return self.position.query.position <= other.query.position
