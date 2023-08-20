5e828b6e9cd861462ad4b436c8abaa995de7dd0c
mikoar <myyq@interia.pl>
2023-08-20 13:58:14 +0200

    score added to Peak

--- src/correlation/peak.py
+++ src/correlation/peak.py
@@ -4,11 +4,12 @@ from __future__ import annotations
 class Peak:
     null: Peak
 
-    def __init__(self, position: int, height: float, leftBase: int = 0, rightBase: int = 0) -> None:
+    def __init__(self, position: int, height: float, leftBase: int = 0, rightBase: int = 0, score: float = 0.) -> None:
         self.position = position
         self.height = height
         self.leftProminenceBasePosition = leftBase
         self.rightProminenceBasePosition = rightBase
+        self.score = score
 
     @property
     def width(self):
