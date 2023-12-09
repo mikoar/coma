0807a176233cb31e2f9297b3699a21924b538b3b
mikoar <myyq@interia.pl>
2023-12-09 19:41:04 +0100

    refactored AlignmentSegmentsFactory to improve readability

--- src/alignment/segments_factory.py
+++ src/alignment/segments_factory.py
@@ -15,25 +15,60 @@ class AlignmentSegmentsFactory:
         self.breakSegmentThreshold = breakSegmentThreshold
 
     def getSegments(self, positions: List[ScoredAlignmentPosition], peak: Peak) -> List[AlignmentSegment]:
-        start = end = 0
-        currentScore = 0
-        resultSegments = []
-        segmentWithMaxScore = EmptyAlignmentSegment(peak, positions)
+        return _AlignmentSegmentBuilder(
+            self.minScore,
+            self.breakSegmentThreshold,
+            positions,
+            peak).getSegments()
 
-        alignmentEnd = len(positions) - 1
-        while end <= alignmentEnd:
-            currentScore += positions[end].score
-            if currentScore > max(0., segmentWithMaxScore.segmentScore - self.breakSegmentThreshold):
-                end += 1
-                if currentScore > segmentWithMaxScore.segmentScore:
-                    segmentWithMaxScore = AlignmentSegment.create(positions[start:end], currentScore, peak, positions)
+
+class _AlignmentSegmentBuilder:
+    def __init__(self,
+                 minScore: float,
+                 breakSegmentThreshold: float,
+                 positions: List[ScoredAlignmentPosition],
+                 peak: Peak):
+        self.minScore = minScore
+        self.breakSegmentThreshold = breakSegmentThreshold
+        self.positions = positions
+        self.peak = peak
+        self.currentSegmentStart = 0
+        self.extendedSegmentEndPosition = 0
+        self.extendedSegmentScore = 0
+        self.currentSegment = EmptyAlignmentSegment(peak, positions)
+        self.resultSegments = []
+
+    def getSegments(self) -> List[AlignmentSegment]:
+        alignmentEnd = len(self.positions) - 1
+        while self.extendedSegmentEndPosition <= alignmentEnd:
+            self.extendedSegmentScore += self.positions[self.extendedSegmentEndPosition].score
+            if self.__extendedSegmentScoreFellBelowBreakSegmentThreshold():
+                self.__breakSegment()
             else:
-                if segmentWithMaxScore.segmentScore >= self.minScore:
-                    resultSegments.append(segmentWithMaxScore)
-                    segmentWithMaxScore = EmptyAlignmentSegment(peak, positions)
-                start = end = end + 1
-                currentScore = 0
-        if segmentWithMaxScore.segmentScore >= self.minScore:
-            resultSegments.append(segmentWithMaxScore)
+                self.extendedSegmentEndPosition += 1
+                self.__acceptExtendedSegmentIfScoreIsImproved()
 
-        return resultSegments or [EmptyAlignmentSegment(peak, positions)]
+        self.__addCurrentSegmentToResultIfScoreIsEnough()
+
+        return self.resultSegments or [EmptyAlignmentSegment(self.peak, self.positions)]
+
+    def __extendedSegmentScoreFellBelowBreakSegmentThreshold(self):
+        return self.extendedSegmentScore <= max(0., self.currentSegment.segmentScore - self.breakSegmentThreshold)
+
+    def __breakSegment(self):
+        self.__addCurrentSegmentToResultIfScoreIsEnough()
+        self.currentSegmentStart = self.extendedSegmentEndPosition = self.extendedSegmentEndPosition + 1
+        self.extendedSegmentScore = 0
+
+    def __addCurrentSegmentToResultIfScoreIsEnough(self):
+        if self.currentSegment.segmentScore >= self.minScore:
+            self.resultSegments.append(self.currentSegment)
+            self.currentSegment = EmptyAlignmentSegment(self.peak, self.positions)
+
+    def __acceptExtendedSegmentIfScoreIsImproved(self):
+        if self.extendedSegmentScore > self.currentSegment.segmentScore:
+            self.currentSegment = AlignmentSegment.create(
+                self.positions[self.currentSegmentStart:self.extendedSegmentEndPosition],
+                self.extendedSegmentScore,
+                self.peak,
+                self.positions)
